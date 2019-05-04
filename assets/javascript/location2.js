cdba4c88c050d04ac34b1a9cf3ce4bc3b5d5b3b7
sdw03c <sharn.dwong@gmail.com>
2019-05-04 02:54:38 -0400

    for buttons to store events data

--- assets/javascript/location2.js
+++ assets/javascript/location2.js
@@ -83,15 +83,15 @@ function moreWikiInfo(extract){
        // var description = $("<h2 class='extract'>" + extract + "</h2>")
         
        
-        wikiResult.append("<li ><img src=" + imgURL + "><div style='max-width:300px' class='overlay'><h2 style='max-width:300px'>" + title + "</h2></div><caption style='max-width:300px;'><h5 style='max-width:300px; padding: 20px;background-color:white'>" + extract + "<button id='location-button' data-event=" + title +  " class='btn btn-success'>Select</button></h5></caption></li>")
-     //  $("#location-button").attr('data-event', title)
+       wikiResult.html("<li ><img src=" + imgURL + "><div style='max-width:300px' class='overlay'><h2 style='max-width:300px'>" + title + "</h2></div><caption style='max-width:300px;'><h5 style='max-width:300px; padding: 20px;background-color:white'>" + extract + "<button id='location-button' class='btn btn-success'>Select</button></h5></caption></li>")
+       $("#location-button").attr('data-event',title)
        $("#location-button").on("click",function(){
         var getToEventPage = $(this).attr("data-event")
         console.log($(this).attr("data-event"))
-     // var url = "index.html?event="+getToEventPage+"";
+      var url = "index.html?event="+getToEventPage+"";
       localStorage.setItem("storageEventName", getToEventPage);
-   //   console.log(url)
-  //   window.location.href = url
+      console.log(url)
+     window.location.href = url
        })
                  // .prependTo('#wiki-snippet')         
                     //$("#sharonTests1").html(location[0])
